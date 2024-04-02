47d404dee02f2555d913860063ef474d555a311f
RoosjenP <80770320+RoosjenP@users.noreply.github.com>
2024-04-02 12:16:50 +0200

    function to create distance raster fixed

--- R/create_threshold_raster.R
+++ R/create_threshold_raster.R
@@ -2,14 +2,13 @@
 #'
 #' @param working_directory The directory where the folder calibration output will be stored. This directory should exist.
 #' @param aoi_name The folder where the folder calibration output will be stored.
-#' @param country_code The three-letter code of the code as we apply it, e.g.: The Netherlands = 'NLA', Kenya = 'KEA'. Case sensitive, so use capitols.
 #' @param distance_threshold The distance at which points can be considered a cluster (0.15 = default).
 #' @param country_shapefile Name of the admin_1 level of the country shapefile (e.g., 'NLD_adm1.shp').
 #' @param current_calibration_points The link (path + filename) to the file of our current calibration points with their corresponding covariable data.
 #' @param plot_resolution Resolution for plotting of distance preview. 300 is default.
 #' @param plot_width Plot width in pixels for plotting of distance preview. Set in range between 2000 - 6000.
 #' @param plot_heigth Plot heigth in pixels  for plotting of distance preview. Set in range between 2000 - 6000.
-#' @returns This creates a map of the AOI showing the distance in feature space with our calibration points. A raster and a preview of it will be returned.
+#' @returns This creates a map of the AOI showing the distance in feature space with our calibration points. A raster and a preview of it will be returned. NOTE: this map can only be generated for countries countries that already have calibration points.
 #' @import terra
 #' @examples
 #' create_threshold_raster(working_directory = 'D:/calibration/projects',
@@ -26,7 +25,6 @@
 # function create raster of distance between AOI and coveriables
 create_threshold_raster <- function(working_directory,
                                     aoi_name,
-                                    country_code,
                                     distance_threshold,
                                     current_calibration_points,
                                     country_shapefile,
@@ -44,8 +42,8 @@ create_threshold_raster <- function(working_directory,
 
   # read covariable data and rename some columns
   dat <- read.csv(paste0(working_directory, '/', aoi_name, '/aoi/', aoi_name, '.csv'))
-  xy <- dat[,c(2,3)]
-  dat <- dat[,c(4:ncol(dat))]
+  xy_aoi <- dat[,c(2,3)]
+  dat_aoi <- dat[,c(4:ncol(dat))]
   # colnames(dat) <- gsub('_15.30cm_mean', '', colnames(dat))
   # colnames(dat) <- gsub('_0.30cm_mean', '', colnames(dat))
 
@@ -53,21 +51,26 @@ create_threshold_raster <- function(working_directory,
   calib <- read.csv(file=current_calibration_points) # extracted from full layers
   calib <- calib[complete.cases(calib),]
   calib_barcode_xy <- data.frame(calib[,c(1:3)])
-  calib <- calib[,c(4:ncol(calib))]
+  dat_calib <- calib[,c(4:ncol(calib))]
   # colnames(calib) <- gsub('_15.30cm_mean', '', colnames(calib))
   # colnames(calib) <- gsub('_0.30cm_mean', '', colnames(calib))
 
   # calibration data and coords
   xy_calib <- data.frame(calib[,c(1:3)])
-  dat_calib <- calib[,c(4:ncol(calib))]
 
   # get scale factors
   minmax_min <- apply(dat_calib,2,min)
   minmax_max <- apply(dat_calib,2,max)
   min_max <- data.frame(min=minmax_min, max=minmax_max)
 
+
+  all_calibration_points <- vect(xy_calib, geom=c('longitude', 'latitude'), crs='EPSG:4326', keepgeom=FALSE)
+  country_calibration_points <- terra::intersect(all_calibration_points, complete_country)
+  barcode_country_calibration_points <- country_calibration_points$SampleId
+
+
   # get only calibration points of country
-  idx <- calib[,1] %like% 'KEA'
+  idx <- calib[,1] %in% barcode_country_calibration_points
 
   xy_calib <- xy_calib[idx,]
   dat_calib <- dat_calib[idx,]
@@ -77,8 +80,8 @@ create_threshold_raster <- function(working_directory,
   dat_calib <- sweep(nom, 2, (min_max$max - min_max$min), '/')
 
   # scale data in aoi
-  nom <- sweep(dat, 2, min_max$min, '-')
-  dat <- sweep(nom, 2, (min_max$max - min_max$min), '/')
+  nom <- sweep(dat_aoi, 2, min_max$min, '-')
+  dat_aoi <- sweep(nom, 2, (min_max$max - min_max$min), '/')
 
   # loop through each calibration point
   n_calib <- nrow(dat_calib)
@@ -90,10 +93,7 @@ create_threshold_raster <- function(working_directory,
     cat('-')
     current_calib <- dat_calib[i,]
 
-    print(colnames(current_calib))
-    print(colnames(dat))
-
-    distance <- t(fields::rdist(current_calib, dat))
+    distance <- t(fields::rdist(current_calib, dat_aoi))
     distances <- cbind(distance, distances)
 
     if(i %% 10 == 0){
