f8da92859fbda7ed3b2b6ea6b3b2b547ff8a357e
RoosjenP <80770320+RoosjenP@users.noreply.github.com>
2024-04-03 10:18:30 +0200

    added functionality to create_threshold_raster

--- R/create_threshold_raster.R
+++ R/create_threshold_raster.R
@@ -3,17 +3,17 @@
 #' @param working_directory The directory where the folder calibration output will be stored. This directory should exist.
 #' @param aoi_name The folder where the folder calibration output will be stored.
 #' @param distance_threshold The distance at which points can be considered a cluster (0.15 = default).
+#' @param n_selected_pixels The number of pixels to take in the AOI to calculate closest calibration points (default = 5000).
 #' @param country_shapefile Name of the admin_1 level of the country shapefile (e.g., 'NLD_adm1.shp').
 #' @param current_calibration_points The link (path + filename) to the file of our current calibration points with their corresponding covariable data.
 #' @param plot_resolution Resolution for plotting of distance preview. 300 is default.
 #' @param plot_width Plot width in pixels for plotting of distance preview. Set in range between 2000 - 6000.
 #' @param plot_heigth Plot heigth in pixels  for plotting of distance preview. Set in range between 2000 - 6000.
-#' @returns This creates a map of the AOI showing the distance in feature space with our calibration points. A raster and a preview of it will be returned. NOTE: this map can only be generated for countries countries that already have calibration points.
+#' @returns This creates a map of the AOI showing the distance in feature space with our calibration points. A raster and a preview of it will be returned. When no calibration points are present in the target country, the closest in feature space will be used.
 #' @import terra
 #' @examples
 #' create_threshold_raster(working_directory = 'D:/calibration/projects',
 #'                         aoi_name = 'The_Netherlands',
-#'                         country_code = 'NLA',
 #'                         distance_threshold = 0.15,
 #'                         current_calibration_points = 'D:/calibration/data/calibration_points/gps_covars.csv',
 #'                         country_shapefile = 'D:/calibration/data/admin_regions/NLD_adm1.shp',
@@ -26,6 +26,7 @@
 create_threshold_raster <- function(working_directory,
                                     aoi_name,
                                     distance_threshold,
+                                    n_selected_pixels,
                                     current_calibration_points,
                                     country_shapefile,
                                     plot_resolution,
@@ -39,21 +40,18 @@ create_threshold_raster <- function(working_directory,
   # read admin shapefile
   complete_adm <- vect(country_shapefile)
   complete_country <- terra::aggregate(complete_adm, dissolve=T)
+  complete_country <- terra::vect('C:/Users/peter/OneDrive - SPRINGG/Projects/R/Argentina/data/shapefiles/ARG_adm0.shp')
 
   # read covariable data and rename some columns
   dat <- read.csv(paste0(working_directory, '/', aoi_name, '/aoi/', aoi_name, '.csv'))
   xy_aoi <- dat[,c(2,3)]
   dat_aoi <- dat[,c(4:ncol(dat))]
-  # colnames(dat) <- gsub('_15.30cm_mean', '', colnames(dat))
-  # colnames(dat) <- gsub('_0.30cm_mean', '', colnames(dat))
 
   # read all data and fix column names
   calib <- read.csv(file=current_calibration_points) # extracted from full layers
   calib <- calib[complete.cases(calib),]
   calib_barcode_xy <- data.frame(calib[,c(1:3)])
   dat_calib <- calib[,c(4:ncol(calib))]
-  # colnames(calib) <- gsub('_15.30cm_mean', '', colnames(calib))
-  # colnames(calib) <- gsub('_0.30cm_mean', '', colnames(calib))
 
   # calibration data and coords
   xy_calib <- data.frame(calib[,c(1:3)])
@@ -64,16 +62,10 @@ create_threshold_raster <- function(working_directory,
   min_max <- data.frame(min=minmax_min, max=minmax_max)
 
 
+  # check if there are any calibration points in the country
   all_calibration_points <- vect(xy_calib, geom=c('longitude', 'latitude'), crs='EPSG:4326', keepgeom=FALSE)
   country_calibration_points <- terra::intersect(all_calibration_points, complete_country)
-  barcode_country_calibration_points <- country_calibration_points$SampleId
 
-
-  # get only calibration points of country
-  idx <- calib[,1] %in% barcode_country_calibration_points
-
-  xy_calib <- xy_calib[idx,]
-  dat_calib <- dat_calib[idx,]
 
   # scale calibration data
   nom <- sweep(dat_calib, 2, min_max$min, '-')
@@ -82,6 +74,59 @@ create_threshold_raster <- function(working_directory,
   # scale data in aoi
   nom <- sweep(dat_aoi, 2, min_max$min, '-')
   dat_aoi <- sweep(nom, 2, (min_max$max - min_max$min), '/')
+
+
+  # if there are points
+  if(length(country_calibration_points) != 0){
+
+    #print status
+    print(paste0(length(country_calibration_points, ' calibration points found in target country')))
+
+          # get barcodes
+          barcode_country_calibration_points <- country_calibration_points$SampleId
+
+          # get only calibration points of country
+          idx <- calib[,1] %in% barcode_country_calibration_points
+
+  } else {
+
+    #print status
+    print(paste0('No calibration points found in target country. Finding the closest calibration points in feature space'))
+
+    n_total_pixels <- c(1:nrow(dat_aoi))
+
+    if(length(n_total_pixels) < n_selected_pixels){
+      n_selected_pixels <- nrow(dat_aoi)
+    }
+
+    # take random sample from AOI
+    set.seed(42)
+    idx_selected_pixels <- sample(n_total_pixels, n_selected_pixels, replace=FALSE)
+    dat_aoi_sub <- dat_aoi[idx_selected_pixels,]
+
+    # calculate distance matrix
+    distanceMatrix <- fields::rdist(dat_aoi_sub, dat_calib)
+    minDist <- Rfast::rowMins(distanceMatrix, value=T)
+
+    # find closest calibration points to pixels in AOI
+    idxs <- NULL
+    for(i in 1:nrow(distanceMatrix)){
+
+      idx <- which(distanceMatrix[i,] == minDist[i])
+      idxs <- c(idxs, idx)
+
+    }
+
+    # find index of closest calibration points
+    barcode_closest_calibration_points <- unique(xy_calib[idxs,]$SampleId)
+    idx <- calib[,1] %in% barcode_closest_calibration_points
+
+  }
+
+  ## subset calibration points
+  xy_calib <- xy_calib[idx,]
+  dat_calib <- dat_calib[idx,]
+
 
   # loop through each calibration point
   n_calib <- nrow(dat_calib)
@@ -141,6 +186,7 @@ create_threshold_raster <- function(working_directory,
   terra::plot(complete_country, add=T, lwd=2)
   terra::points(xy_calib$longitude, xy_calib$latitude,
                 pch = 21, cex=0.5, col="black", bg="yellow")
+
 
   dev.off()
 
